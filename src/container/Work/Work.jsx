22fbe1cb78bc85766e477cff28c50bcecef14a53
Timmydee <dahunsitimmy@gmail.com>
2022-12-02 09:08:29 -0800

    update

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Work.scss'
 import {images} from '../../constant'
 import {FiArrowRight} from 'react-icons/fi'
-import {Link} from 'react-router-dom'
+// import {Link} from 'react-router-dom'
 
 import Slider from 'react-slick'
 
